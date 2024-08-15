002aa817eab4bcf9b67d563b3ea83bba1694fdac
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2024-08-15 02:21:34 -0500

    Typos lmao

--- source/states/menus/freeplay/RemakedFreeplayMenu.hx
+++ source/states/menus/freeplay/RemakedFreeplayMenu.hx
@@ -23,16 +23,16 @@ class RemakedFreeplayMenu extends MusicBeatState
 		['Devilish Deal', 1, "minnie", FlxColor.fromRGB(65, 88, 94), 'obscurity', 'EASY', FlxColor.WHITE],
 		['Isolated', 1, "avier", FlxColor.fromRGB(60, 60, 60), 'obscurity', 'EASY', FlxColor.WHITE],
 		['Lunacy', 1, "lunaavier", FlxColor.fromRGB(69, 54, 54), 'obscurity', 'NORMAL', FlxColor.fromRGB(255, 220, 220)],
-		['Delusional', 3, 'deluavier', FlxColor.fromRGB(79, 32, 32), 'FR3SHMoure', 'INSANE', FlxColor.fromRGB(255, 110, 110)]
+		['Delusional', 1, 'deluavier', FlxColor.fromRGB(79, 32, 32), 'FR3SHMoure', 'INSANE', FlxColor.fromRGB(255, 110, 110)]
 	];
 
 	//i think you get the idea
 	var legacySongs:Array<Array<Dynamic>> = [
-		['Isolated-Legacy', 3, (GameData.legacyILock != 'unlocked' && GameData.legacyILock != 'beaten' ? 'untouched-song' : 'mickey-legacy'), FlxColor.fromRGB(60, 60, 60), 'Toko & obscurity', 'NORMAL', FlxColor.fromRGB(255, 220, 220)]
+		['Isolated-Legacy', 3, (GameData.legacyILock != 'unlocked' && GameData.legacyILock != 'beaten' ? 'mysteryfp' : 'avierlegacy'), FlxColor.fromRGB(60, 60, 60), 'Toko & obscurity', 'NORMAL', FlxColor.fromRGB(255, 220, 220)]
 	];
 
 	var freeplaySongs:Array<Array<Dynamic>> = [
-		['Hunted', 3, (GameData.huntedLock != 'unlocked' && GameData.huntedLock != 'beaten' ? 'mysteryfp' : 'goofy'), FlxColor.fromRGB(94, 28, 35), 'JBlitz', 'NORMAL', FlxColor.fromRGB(255, 220, 220)]
+		['Hunted', 2, (GameData.huntedLock != 'unlocked' && GameData.huntedLock != 'beaten' ? 'mysteryfp' : 'goofy'), FlxColor.fromRGB(94, 28, 35), 'JBlitz', 'NORMAL', FlxColor.fromRGB(255, 220, 220)]
 	];
 
 
