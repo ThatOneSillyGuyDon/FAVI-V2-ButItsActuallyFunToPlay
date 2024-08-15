f555f686af5cb3fd3ac83578667619f09fc227c5
DEMOLITIONDON96 <87618246+DEMOLITIONDON96@users.noreply.github.com>
2024-08-15 02:12:33 -0500

    Perhaps try this for later?

--- source/states/menus/freeplay/RemakedFreeplayMenu.hx
+++ source/states/menus/freeplay/RemakedFreeplayMenu.hx
@@ -19,19 +19,20 @@ class RemakedFreeplayMenu extends MusicBeatState
 		}
 	*/
 
-	var storyMenuSongs = [
-		'delvish-deal',
-		'isolated',
-		'lunacy',
-		'delusional'
+	var storyMenuSongs:Array<Array<Dynamic>> = [
+		['Devilish Deal', 1, "minnie", FlxColor.fromRGB(65, 88, 94), 'obscurity', 'EASY', FlxColor.WHITE],
+		['Isolated', 1, "avier", FlxColor.fromRGB(60, 60, 60), 'obscurity', 'EASY', FlxColor.WHITE],
+		['Lunacy', 1, "lunaavier", FlxColor.fromRGB(69, 54, 54), 'obscurity', 'NORMAL', FlxColor.fromRGB(255, 220, 220)],
+		['Delusional', 3, 'deluavier', FlxColor.fromRGB(79, 32, 32), 'FR3SHMoure', 'INSANE', FlxColor.fromRGB(255, 110, 110)]
 	];
 
-	var legacySongs = [
-		''
+	//i think you get the idea
+	var legacySongs:Array<Array<Dynamic>> = [
+		['Isolated-Legacy', 3, (GameData.legacyILock != 'unlocked' && GameData.legacyILock != 'beaten' ? 'untouched-song' : 'mickey-legacy'), FlxColor.fromRGB(60, 60, 60), 'Toko & obscurity', 'NORMAL', FlxColor.fromRGB(255, 220, 220)]
 	];
 
-	var freeplaySongs = [
-		''
+	var freeplaySongs:Array<Array<Dynamic>> = [
+		['Hunted', 3, (GameData.huntedLock != 'unlocked' && GameData.huntedLock != 'beaten' ? 'mysteryfp' : 'goofy'), FlxColor.fromRGB(94, 28, 35), 'JBlitz', 'NORMAL', FlxColor.fromRGB(255, 220, 220)]
 	];
 
 
