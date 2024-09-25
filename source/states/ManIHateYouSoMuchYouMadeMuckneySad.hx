2331764b49fffc559d3ffb01a2be7af06491bce8
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2024-09-25 11:47:43 -0300

    other message

--- source/states/ManIHateYouSoMuchYouMadeMuckneySad.hx
+++ source/states/ManIHateYouSoMuchYouMadeMuckneySad.hx
@@ -18,6 +18,8 @@ import flixel.FlxG;
 /*why everyone forgot about this
 ...... should I do the funnies here? - malyplus/*
 
+// no i didn't forgot im just WAITING FOR THE ASSETS
+
 /**
  * why did you left his birthday
  */
