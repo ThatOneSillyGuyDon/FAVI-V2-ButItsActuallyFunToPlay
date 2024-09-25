4f464dd80a12ee8bdc0416b4d2fb275f16ac3fa2
JustJasonLol <98182734+JustJasonLol@users.noreply.github.com>
2024-09-24 22:00:01 -0300

    maybe

--- source/Main.hx
+++ source/Main.hx
@@ -99,7 +99,28 @@ class Main extends Sprite
 		#if CRASH_HANDLER
 		Lib.current.loaderInfo.uncaughtErrorEvents.addEventListener(UncaughtErrorEvent.UNCAUGHT_ERROR, onCrash);
 		#end
+
+		FlxG.signals.gameResized.add(function (w, h) {
+			if (FlxG.cameras != null) {
+			  for (cam in FlxG.cameras.list) {
+			   if (cam != null && cam.filters != null)
+				   resetSpriteCache(cam.flashSprite);
+			  }
+		   }
+
+		   if (FlxG.game != null)
+		    @:privateAccess {
+				FlxG.game.__cacheBitmap = null;
+				FlxG.game.__cacheBitmapData = null;
+		}
+	   });
 	}
+
+	static function resetSpriteCache(sprite:Sprite):Void {
+		@:privateAccess {
+				sprite.__cacheBitmap = null;
+			sprite.__cacheBitmapData = null;
+		}
 
 	// Code was entirely made by sqirra-rng for their fnf engine named "Izzy Engine", big props to them!!!
 	// very cool person for real they don't get enough credit for their work
