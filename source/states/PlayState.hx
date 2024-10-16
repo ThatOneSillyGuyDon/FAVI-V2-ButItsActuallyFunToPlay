3a8b68de25dbbe650a8b120660d85a879919bc60
MalyPlus <malyplusofficial@gmail.com>
2024-10-16 19:30:04 +0200

    added what yama wanted

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -59,6 +59,11 @@ typedef FlashingSettings =
 
 class PlayState extends MusicBeatState
 {
+	// type false to turn it off the new feature that i added!
+	var isItDelvish:Bool = true;
+
+	var delvishShit:Bool = false
+
 	public static var STRUM_X = 42;
 	public static var STRUM_X_MIDDLESCROLL = -278;
 	var middlescroll:Bool = false;
@@ -576,6 +581,7 @@ class PlayState extends MusicBeatState
 			'NOTE_UP',
 			'NOTE_RIGHT'
 		];
+
 
 		//Ratings
 		ratingsData.push(new Rating('sick')); //default rating
@@ -1579,6 +1585,13 @@ class PlayState extends MusicBeatState
 		{
 			case "Isolated" | "Devilish Deal" | "Lunacy" | "Delusional" | "Hunted" | "Twisted Grins" | "Laugh Track" |  "Isolated Old" | "Isolated Beta" | "Isolated Legacy" | "Lunacy Legacy" | "Delusional Legacy" | "Hunted Legacy" | "Birthday":
 				introSoundsSuffix = "-cartoon";
+
+				
+				if (isItDelvish)
+				{
+					delvishShit = true;
+				}
+
 			case "Cycled Sins Legacy" | "Cycled Sins":
 				introSoundsSuffix = "-sins";
 			case "Malfunction":
@@ -5854,7 +5867,7 @@ class PlayState extends MusicBeatState
 
 	public var isDead:Bool = false; //Don't mess with this on Lua!!!
 	function doDeathCheck(?skipHealthCheck:Bool = false) {
-		if (((skipHealthCheck && instakillOnMiss) || healthThing <= 0) && !practiceMode && !isDead)
+		if (((skipHealthCheck && instakillOnMiss) || healthThing <= 0) && !practiceMode && !isDead && !delvishShit) // Yama wanted the funni in - MalyPlus
 		{
 			var ret:Dynamic = callOnLuas('onGameOver', [], false);
 			if(ret != FunkinLua.Function_Stop) {
