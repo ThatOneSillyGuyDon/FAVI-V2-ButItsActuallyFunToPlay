700355c2435ec763cf805c21cb65213c9b5cdb7e
ThatOneSillyGuy // Don <87618246+DEMOLITIONDON96@users.noreply.github.com>
2025-06-30 23:17:39 -0500

    me personally, I like having this as the icon

--- source/modcharting/ModchartEditorState.hx
+++ source/modcharting/ModchartEditorState.hx
@@ -86,7 +86,7 @@ class ModchartEditorEvent extends FlxSprite
         frames = Paths.getSparrowAtlas("ui skins/" + utilities.Options.getData("uiSkin") + "/arrows/default", 'shared');
         animation.addByPrefix('note', 'left0');
         #elseif PSYCH
-        loadGraphic(Paths.image('eventArrow'));
+        loadGraphic(Paths.image('eventArrowModchart'));
         #else
         frames = Paths.getSparrowAtlas('NOTE_assets');
         animation.addByPrefix('note', 'purple0');
