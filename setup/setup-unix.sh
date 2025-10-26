383aa2a5ead768df25d86a2b86ddb5d15fd7925e
GooberManGoober <123832142+GooberManGoober@users.noreply.github.com>
2025-10-26 10:37:06 -0400

    ugh

---
+++ setup/setup-unix.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# SETUP FOR MAC AND LINUX SYSTEMS!!!
+#
+# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
+# https://haxe.org/download/version/4.2.5/
+haxelib set lime 8.2.2
+haxelib set openfl 9.4.1
+haxelib set flixel 5.6.1
+haxelib set flixel-addons 3.2.2
+haxelib set flixel-ui 2.5.0
+haxelib set flixel-tools 1.5.1
+haxelib set SScript 7.7.0
+haxelib git hxCodec https://github.com/polybiusproxy/hxCodec
+haxelib set tjson 1.4.0
+haxelib set hxvlc 1.2.0
+haxelib remove flxanimate
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+haxelib set hxdiscord_rpc 1.2.4
+haxelib set hxcpp 4.3.2
