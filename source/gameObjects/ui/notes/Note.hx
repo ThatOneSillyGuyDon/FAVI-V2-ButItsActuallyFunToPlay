3b025a3e73ff957593481fe6ecd85e5705545b62
Goober Man <123832142+GooberManGoober@users.noreply.github.com>
2025-12-16 16:24:03 -0500

    Update Note.hx

--- source/gameObjects/ui/notes/Note.hx
+++ source/gameObjects/ui/notes/Note.hx
@@ -693,17 +693,7 @@ class Note extends FlxSkewedSprite
 		}
 		else animation.addByPrefix(colArray[noteData] + 'Scroll', colArray[noteData] + '0');
 
-		var s = 0.7;
-		switch (PlayState.SONG.song)
-		{
-			case "Isolated Old" | "Isolated Beta" | "Isolated Legacy" | "Lunacy Legacy" | "Delusional Legacy" | "Hunted Legacy" | "Malfunction Legacy" | "Cycled Sins Legacy" | "Mercy Legacy" | "Delutrance" | "Malfunction":
-				s = 0.7;
-			case "Dont Cross" | "Mercy":
-			   s = 0.64;
-			default:
-				s = 0.6;
-		}
-		setGraphicSize(Std.int(width * s));
+		setGraphicSize(Std.int(width * 0.7));
 		updateHitbox();
 	}
 
