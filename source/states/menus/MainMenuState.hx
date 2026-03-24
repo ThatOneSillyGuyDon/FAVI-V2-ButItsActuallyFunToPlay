89df63943cc6311fdcd074e92e42847eeec268c6
GooberManGoober <123832142+GooberManGoober@users.noreply.github.com>
2026-03-23 23:42:17 -0400

    some changes

--- source/states/menus/MainMenuState.hx
+++ source/states/menus/MainMenuState.hx
@@ -101,7 +101,7 @@ class MainMenuState extends MusicBeatState
 		"women.",
 		"men.",
 		"Blame Goober for making me delusional",
-		"Fun fact: away3d is an library that makes our mod work, deleting it would result on us not being to compile no more" // change if needed.
+		"Fun fact: away3d is a library that makes our mod work, deleting it would result on us not being to compile no more" // change if needed.
 	];
 
 	override function create()
