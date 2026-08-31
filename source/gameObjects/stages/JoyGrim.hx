8257d70287b0977325d2b5ad5b9c0c30196ee0ee
MalyPlus <101642961+MalyPlus@users.noreply.github.com>
2026-08-31 13:05:26 +0200

    I'm gonna repeat to everyone that sees this but PLEASE do not be sharing this anywhere.

--- source/gameObjects/stages/JoyGrim.hx
+++ source/gameObjects/stages/JoyGrim.hx
@@ -3,7 +3,7 @@ package gameObjects.stages;
 class JoyGrim extends BaseStage
 {
 	public var offsetTwn:FlxTween;
-
+	var clubhouse:FlxSprite;
 	override function create()
 	{
 		game.defaultCamZoom = 0.75;
@@ -11,7 +11,7 @@ class JoyGrim extends BaseStage
 		PlayState.isGreyscale = false;
 
 		// you may wonder why i did like that.. simple, PATHS IMAGES DIDNT WANT TO LOAD CORRECTLY ! 
-		var clubhouse:FlxSprite = new FlxSprite(-770, -650).loadGraphic(Paths.image('favi/stages/clubhouse/images/clubhouse'));
+		clubhouse = new FlxSprite(-770, -650).loadGraphic(Paths.image('favi/stages/clubhouse/images/clubhouse'));
 		clubhouse.scale.set(1.4, 1.4);
 		add(clubhouse);
 	}
@@ -25,7 +25,30 @@ class JoyGrim extends BaseStage
 		super.update(elapsed);
 	}
 
-	override function createPost()
-	{
+	override function createPost() {
+		game.camBars.fade(FlxColor.BLACK, 0.0001);
+		game.camHUD.alpha = 0;
+
+	}
+	// so like the event where the background goes insane right? i ate it.
+	
+	override function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float){
+		// so you may wonder why im not going to make an eventName instead?
+		// simple, im fucking lazy + the mod aint going to use this function anyway
+		switch(value1){
+			case "evilShi":
+				FlxTween.color(clubhouse, 0.2, FlxColor.WHITE, FlxColor.fromRGB(255, 17, 0));
+				FlxTween.color(game.iconP2, 0.2, FlxColor.WHITE, FlxColor.fromRGB(255, 17, 0));
+
+				for (i in 0...4) FlxTween.color(game.opponentStrums.members[i], 0.2, FlxColor.WHITE, FlxColor.fromRGB(255, 17, 0));
+			case "yoMute":
+				FlxG.sound.music.volume = 0;
+            	game.camGame.visible = game.camHUD.visible = false;
+			case 'yofuck':
+				game.isCameraOnForcedPos = true;
+            	FlxTween.tween(game.camFollow, {x: (game.dad.getMidpoint().x + 150) + (game.dad.cameraPosition[0] + game.opponentCameraOffset[0]) - 30}, 4, {ease: FlxEase.sineInOut});
+
+		}
+
 	}
 }
